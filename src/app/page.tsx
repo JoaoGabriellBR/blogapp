0b8f77192ea88dd8861ee0fe7930f4df8f929152
João Gabriel <joaoname9@gmail.com>
2023-08-05 06:13:09 -0300

    feat: adding newsData properties

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,17 @@
 import Post from '@/components/Post'
 import fetchNewsData from '@/services/fetchNewsData'
 import { useState, useEffect } from 'react'
+import { AiOutlineArrowDown } from "react-icons/ai";
 
 export default function Home() {
 
-  const [newsData, setNewsData] = useState(undefined);
+  const [newsData, setNewsData] = useState([]);
+
+  const [visibleItems, setVisibleItems] = useState(3); // Number of items to display initially
+
+  const handleSeeMore = () => {
+    setVisibleItems((prev: any) => prev + 3); // Increase the number of visible items when "Ver mais" is clicked
+  };
 
   const loadData = async () => {
     const response = await fetchNewsData();
@@ -17,11 +24,27 @@ export default function Home() {
     loadData();
   }, [])
 
+
   return (
     <>
-      <main>
-        <Post newsData={newsData} />
-      </main>
+    <main className='w-screen h-screen flex flex-col justify-start items-start'>
+
+      <h1 className='text-[3rem] self-start font-bold'>News App</h1>
+
+      <div className='flex flex-row flex-wrap justify-center items-center items-stretch'>
+        <Post newsData={newsData} visibleItems={visibleItems} />
+      </div>
+
+      {newsData?.length > visibleItems && (
+        <button
+          onClick={handleSeeMore}
+          className="inline-flex items-center px-3 py-2 mt-3 text-sm font-medium text-center text-white rounded-lg bg-blue-500"
+        >
+          Ver mais
+          <AiOutlineArrowDown className="ml-1 w-[1rem] h-[1rem] font-bold" />
+        </button>
+      )}
+    </main>
     </>
   )
 }
--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -3,18 +3,31 @@ import { AiOutlineArrowRight } from "react-icons/ai";
 import moment from "moment";
 import "moment/locale/pt-br";
 
-export default function Post({ newsData }: any) {
+interface NewsItem {
+  title: string;
+  urlToImage?: string;
+  publishedAt: string;
+  author: string;
+}
 
-  const formattedDate = (date: string) => moment(date).format('ll');
+interface PropsNewsItem {
+  newsData: NewsItem[];
+  visibleItems: number;
+}
+
+export default function Post({ newsData, visibleItems }: PropsNewsItem) {
+
+  const formattedDate = (date: string) => moment(date).format("ll");
 
   // ADICIONAR PROPS DE TAMANHO, COR E FUNDO DO POST PARA COLOCAR
   // POST EM DESTAQUE E ETC...
 
   return (
     <>
-      {newsData?.map((item: any) => (
+      {newsData?.slice(0, visibleItems).map((item: any) => (
         <>
-          <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-4">
+          <div className="max-w-[20rem] max-h-[30rem] bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-4">
+            
             {item?.urlToImage ? (
               <div className="w-full h-48 my-5">
                 <img
@@ -27,10 +40,10 @@ export default function Post({ newsData }: any) {
 
             <div>
               <h1 className="mb-2 text-[1.2rem] font-bold text-gray-900 dark:text-white">
-                {item?.title}
+               {item?.title?.length >= 50 ? `${item.title?.slice(0,50)}...` : item?.title}
               </h1>
               <p className="mb-3 text-[0.9rem] font-light text-gray-700 dark:text-gray-400">
-                {item?.description}
+                {item?.description?.length >= 120 ? `${item.description?.slice(0,120)}...` : item?.description}
               </p>
 
               <div className="flex flex-row justify-between items-center">
