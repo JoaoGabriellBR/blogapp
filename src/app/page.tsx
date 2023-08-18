862b85c0cb8365c4ffe80db3ff03a32639d42dc7
João Gabriel <joaoname9@gmail.com>
2023-08-18 16:48:39 -0300

    feat: framer motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import fetchNewsData from "@/services/fetchNewsData";
 import { useState, useEffect } from "react";
 import { AiOutlineArrowDown } from "react-icons/ai";
 import { NewsItem, PropsNewsItem } from "@/services/Interfaces/newsItem";
+import { motion } from "framer-motion";
 // import axios from "axios";
 // import { LazyLoadImage } from 'react-lazy-load-image-component';
 // import { LazyLoadComponent } from 'react-lazy-load-image-component';
@@ -46,27 +47,35 @@ export default function Home() {
         <h1>Carregando...</h1>
       ) : (
         <>
-          <div className="flex flex-col justify-start my-16">
-            <h1 className="text-[2.5rem] font-bold mb-10 w-full md:w-7/12">
-              Seu destino ideal para notícias relevantes
-            </h1>
-            <MainPost news={newsData?.filter((i) => i?.multimedia?.length !== 0)[0]} visibleItems={1} />
-          </div>
+            <motion.div
+              className="flex flex-col justify-start my-16"
+              initial={{ opacity: 0, y: 40 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1.3 }}
+            >
+              <h1 className="text-[2.5rem] font-bold mb-10 w-full md:w-7/12">
+                Seu destino ideal para notícias relevantes
+              </h1>
+              <MainPost news={newsData?.filter((i) => i?.multimedia?.length !== 0)[0]} visibleItems={1} />
+            </motion.div>
 
-          <div className="w-full h-full flex flex-col items-start">
+            <div className="w-full h-full flex flex-col items-start">
             <h1 className="text-[1.5rem] font-bold text-start mb-5">
               Últimas novidades
             </h1>
-            <div className="w-full h-[0.1rem] mb-7 bg-neutral-700"></div>
+              <div className="w-full h-[0.1rem] mb-7 bg-neutral-700"></div>
+            
+                <div className="w-full m-0 md:mr-10 mb-4 flex flex-row flex-wrap justify-between items-stretch">
+                  {newsData
+                    ?.slice(0, 10)
+                    ?.filter((item) => item?.multimedia?.length !== 0)
+                    ?.map((news: NewsItem) => (
+                      <>
+                        <Post key={news?._id} news={news} visibleItems={visibleItems} />
+                      </>
+                    ))}
+                </div>
 
-            <div className="w-full m-0 md:mr-10 mb-4 flex flex-row flex-wrap justify-between items-stretch">
-              {newsData
-                ?.slice(0, 10)
-                ?.filter((item) => item?.multimedia?.length !== 0)
-                ?.map((news: NewsItem) => (
-                  <Post key={news?._id} news={news} visibleItems={visibleItems} />
-                ))}
-            </div>
 
             <div className="w-full flex flex-row justify-center items-center">
               {newsData?.length > visibleItems && (
