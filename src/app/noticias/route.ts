c77abb3aebaa0daed6a2f03477154c880230daa1
João Gabriel <joaoname9@gmail.com>
2023-08-22 17:40:04 -0300

    feat: category api

--- src/app/noticias/route.ts
+++ src/app/noticias/route.ts
@@ -1,14 +1,14 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
-
+export async function GET( request: NextRequest) {
+    
+    const category = request.nextUrl.searchParams.get("category");
+    const dataCategory = category !== null ? `&fq=news_desk:${category}` : '';
     const apiKey = "WCwDGgHrj9SFZsmhgzB2d4nvozkkZwOG";
+    const apiURL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?${dataCategory}&api-key=${apiKey}&page=1`;
+    // const apiUrl = `https://api.nytimes.com/svc/archive/v1/2023/1.json?api-key=${apiKey}`;
 
-    const articles = `https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=news_desk:("Science")&api-key=${apiKey}&page=1`;
-
-    const apiUrl = `https://api.nytimes.com/svc/archive/v1/2023/1.json?api-key=${apiKey}`;
-
-    const res = await fetch(articles)
+    const res = await fetch(apiURL)
     const data = await res.json();
     return NextResponse.json({ data })
 }
